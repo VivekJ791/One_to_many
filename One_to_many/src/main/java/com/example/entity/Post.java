cbc466dc27a05427589098e16ad2ef33dfac8a1a
VivekJ791 <135229929+VivekJ791@users.noreply.github.com>
2023-06-02 10:57:22 +0530

    one to many
    table post cannot be created issue

---
+++ One_to_many/src/main/java/com/example/entity/Post.java
@@ -0,0 +1,37 @@
+package com.example.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name="post")
+public class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Integer id;
+	String title;
+	String desc;
+	
+	@OneToMany(mappedBy = "post")
+	List<Comment> comments;
+}
